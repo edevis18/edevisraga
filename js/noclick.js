ccb7073d27aa12aabdee2b5166e402ab5ad6f1d6
edevis18 <74991128+edevis18@users.noreply.github.com>
2025-10-16 19:45:11 -0400

    Add files via upload

--- js/noclick.js
+++ js/noclick.js
@@ -1,29 +1,44 @@
-// Bloquear el clic derecho
+(function() {
+
+    const blockAndRedirect = function() {
+        clearInterval(interval);
+        window.location.href = 'index.html';
+    };
+
     document.addEventListener('contextmenu', function(e) {
         e.preventDefault();
     }, false);
 
-    // Bloquear las teclas F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U y Ctrl+Shift+C
     document.addEventListener('keydown', function(e) {
-        // Bloquear F12
         if (e.keyCode == 123) {
             e.preventDefault();
         }
-        // Bloquear Ctrl+Shift+I (tecla I)
         if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
             e.preventDefault();
         }
-        // Bloquear Ctrl+Shift+J (tecla J)
         if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
             e.preventDefault();
         }
-        // Bloquear Ctrl+U
         if (e.ctrlKey && e.keyCode == 85) {
             e.preventDefault();
         }
-        // Bloquear Ctrl+Shift+C (tecla C)
         if (e.ctrlKey && e.shiftKey && e.keyCode == 67) {
             e.preventDefault();
         }
     }, false);
 
+    const threshold = 160; 
+
+    const interval = setInterval(function() {
+        const start = Date.now();
+        
+        debugger;
+        
+        const end = Date.now();
+        
+   
+        if (end - start > threshold) {
+            blockAndRedirect();
+        }
+    }, 1000); 
+})();
